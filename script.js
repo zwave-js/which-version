9631fff5a8cd3ab01f3f75f148dea0067de7a1e9
Dominic Griesel <dominic.griesel@nabucasa.com>
2026-02-19 13:59:32 +0100

    fix: support new core addon which includes Z-UI

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function stripV(version) {
+	return version?.replace(/^v/, "");
+}
+
 async function fetchDriverLatestVersion() {
 	const response = await fetch("https://api.github.com/repos/zwave-js/node-zwave-js/releases");
 	const responseJson = await response.json();
@@ -5,7 +9,7 @@ async function fetchDriverLatestVersion() {
 }
 
 
-async function fetchHAZUIAddonLatestVersion() {
+async function fetchHAZUIAppLatestVersion() {
 	const response = await fetch("https://api.github.com/repos/hassio-addons/app-zwave-js-ui/releases");
 	const responseJson = await response.json();
 	return responseJson.filter(r => !r.prerelease)[0].tag_name;
@@ -17,7 +21,7 @@ async function fetchZUILatestVersion() {
 	return responseJson.filter(r => !r.prerelease)[0].tag_name;
 }
 
-async function fetchHAZUIAddonZUIVersion(version) {
+async function fetchHAZUIAppZUIVersion(version) {
 	const Dockerfile = await fetch(`https://raw.githubusercontent.com/hassio-addons/app-zwave-js-ui/${version}/zwave-js-ui/Dockerfile`);
 	const DockerfileText = await Dockerfile.text();
 	// ARG ZWAVE_JS_UI_VERSION="11.11.0"
@@ -27,7 +31,7 @@ async function fetchHAZUIAddonZUIVersion(version) {
 	return zuiVersion && !zuiVersion.startsWith("v") ? `v${zuiVersion}` : zuiVersion;
 }
 
-async function fetchHACoreAddonVersion() {
+async function fetchHACoreAppVersion() {
 	const response = await fetch(`https://raw.githubusercontent.com/home-assistant/addons/master/zwave_js/config.yaml`);
 	const responseText = await response.text();
 	// version: 0.1.83
@@ -36,13 +40,14 @@ async function fetchHACoreAddonVersion() {
 	return match?.[1];
 }
 
-async function fetchHACoreAddonDriverVersion() {
+async function fetchHACoreAppZUIVersion() {
 	const response = await fetch(`https://raw.githubusercontent.com/home-assistant/addons/master/zwave_js/build.yaml`);
 	const responseText = await response.text();
-	//  ZWAVEJS_VERSION: 10.22.3
-	const regex = /ZWAVEJS_VERSION: (.*)/gm;
+	//  ZWAVEJS_UI_VERSION: 11.11.0
+	const regex = /ZWAVEJS_UI_VERSION: (.*)/gm;
 	const match = regex.exec(responseText);
-	return match?.[1];
+	const zuiVersion = match?.[1];
+	return zuiVersion && !zuiVersion.startsWith("v") ? `v${zuiVersion}` : zuiVersion;
 }
 
 async function fetchZUIDriverVersion(version) {
@@ -54,38 +59,40 @@ async function fetchZUIDriverVersion(version) {
 async function fetchDriver() {
 	const driverVersion = await fetchDriverLatestVersion();
 
-	document.getElementById("driver__version").innerText = driverVersion;
+	document.getElementById("driver__version").innerText = stripV(driverVersion);
 }
 
-async function fetchHAZUIAddon() {
-	const haAddonVersion = await fetchHAZUIAddonLatestVersion();
-	const haAddonZUIVersion = await fetchHAZUIAddonZUIVersion(haAddonVersion);
-	const zwavejsVersion = await fetchZUIDriverVersion(haAddonZUIVersion);
+async function fetchHAZUIApp() {
+	const haAppVersion = await fetchHAZUIAppLatestVersion();
+	const haAppZUIVersion = await fetchHAZUIAppZUIVersion(haAppVersion);
+	const zwavejsVersion = await fetchZUIDriverVersion(haAppZUIVersion);
 
-	document.getElementById("ha-addon__version").innerText = haAddonVersion;
-	document.getElementById("ha-addon__zui-version").innerText = haAddonZUIVersion;
-	document.getElementById("ha-addon__driver-version").innerText = zwavejsVersion;
+	document.getElementById("ha-app__version").innerText = stripV(haAppVersion);
+	document.getElementById("ha-app__zui-version").innerText = stripV(haAppZUIVersion);
+	document.getElementById("ha-app__driver-version").innerText = stripV(zwavejsVersion);
 }
 
 async function fetchZUI() {
 	const zuiVersion = await fetchZUILatestVersion();
 	const zwavejsVersion = await fetchZUIDriverVersion(zuiVersion);
 
-	document.getElementById("zui__version").innerText = zuiVersion;
-	document.getElementById("zui__driver-version").innerText = zwavejsVersion;
+	document.getElementById("zui__version").innerText = stripV(zuiVersion);
+	document.getElementById("zui__driver-version").innerText = stripV(zwavejsVersion);
 }
 
-async function fetchHACoreAddon() {
-	const addonVersion = await fetchHACoreAddonVersion();
-	const zwavejsVersion = await fetchHACoreAddonDriverVersion();
+async function fetchHACoreApp() {
+	const appVersion = await fetchHACoreAppVersion();
+	const zuiVersion = await fetchHACoreAppZUIVersion();
+	const zwavejsVersion = await fetchZUIDriverVersion(zuiVersion);
 
-	document.getElementById("ha-core__version").innerText = addonVersion;
-	document.getElementById("ha-core__driver-version").innerText = zwavejsVersion;
+	document.getElementById("ha-core__version").innerText = stripV(appVersion);
+	document.getElementById("ha-core__zui-version").innerText = stripV(zuiVersion);
+	document.getElementById("ha-core__driver-version").innerText = stripV(zwavejsVersion);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
 	fetchDriver().catch(console.error);
-	fetchHAZUIAddon().catch(console.error);
+	fetchHAZUIApp().catch(console.error);
 	fetchZUI().catch(console.error);
-	fetchHACoreAddon().catch(console.error);
+	fetchHACoreApp().catch(console.error);
 });
